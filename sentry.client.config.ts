7d213befe0bba0c3b8f72cb3710bbe133e86caa3
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-06-19 23:23:00 +0200

    chore: add Sentry client configuration

---
+++ sentry.client.config.ts
@@ -0,0 +1,10 @@
+// Sentry client configuration
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  tracesSampleRate: 0.2,
+  debug: false,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 0.1,
+});
